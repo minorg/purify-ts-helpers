c524db82121483d73e33618669ddef7e5c267db3
Minor Gordon <minorg@server.fake>
2024-07-30 17:02:06 -0400

    eliminate tab madness

--- AsyncIterables.ts
+++ AsyncIterables.ts
@@ -1,11 +1,11 @@
 export namespace AsyncIterables {
-	export async function toArray<T>(
-		asyncIterable: AsyncIterable<T>,
-	): Promise<readonly T[]> {
-		const values: T[] = [];
-		for await (const value of asyncIterable) {
-			values.push(value);
-		}
-		return values;
-	}
+  export async function toArray<T>(
+    asyncIterable: AsyncIterable<T>,
+  ): Promise<readonly T[]> {
+    const values: T[] = [];
+    for await (const value of asyncIterable) {
+      values.push(value);
+    }
+    return values;
+  }
 }
--- Maybes.ts
+++ Maybes.ts
@@ -1,22 +1,22 @@
 import type { Maybe } from "purify-ts";
 
 export namespace Maybes {
-	export function equals<T>(
-		left: Maybe<T>,
-		right: Maybe<T>,
-		equals: (left: T, right: T) => boolean,
-	) {
-		if (left.isJust()) {
-			if (right.isJust()) {
-				return equals(left.unsafeCoerce(), right.unsafeCoerce());
-			}
-			return false;
-		}
+  export function equals<T>(
+    left: Maybe<T>,
+    right: Maybe<T>,
+    equals: (left: T, right: T) => boolean,
+  ) {
+    if (left.isJust()) {
+      if (right.isJust()) {
+        return equals(left.unsafeCoerce(), right.unsafeCoerce());
+      }
+      return false;
+    }
 
-		if (right.isJust()) {
-			return false;
-		}
+    if (right.isJust()) {
+      return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 }
